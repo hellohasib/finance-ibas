4921596e119cf2d55a78f3769acf9f8afc52e166
ASRAF-UL <asraful8625@gmail.com>
2021-06-19 19:30:00 +0600

    Captured Image API updated.

--- app/controllers/capturedImage.controller.js
+++ app/controllers/capturedImage.controller.js
@@ -1,6 +1,6 @@
 const db = require("../models");
 const capturedImage = db.capturedImages;
-
+const axios = require("axios");
 exports.createCapturedImage = (req, res) => {
   // Validate request
   if (!req.body.image) {
@@ -9,20 +9,41 @@ exports.createCapturedImage = (req, res) => {
     });
     return;
   }
- 
 
   // Create a capturedImage
   const image = {
-    image: req.body.image
+    image: req.body.image,
   };
+  let capturedId;
+  let personRowId;
+  capturedImage
+    .create(image)
+    .then(async (response) => {
+      capturedId = response.dataValues.id;
+      console.log("When capture posted: ", capturedId);
+      
+      await axios
+        .post(`http://localhost:8012/api/person/create`, {
+          nidProvided: null,
+          confidenceScore: null,
+          status: null,
+          capturedImages_id: capturedId,
+        })
+        .then((res) => {
+          
+          personRowId = res.data.id;
+          console.log("Captured: ", res.data);
+          console.log("Captured post response: ", personRowId);
+        })
+        .catch((err) => console.log("error while posting to person api."));
 
-  capturedImage.create(image)
-    .then((data) => {
-      res.send(data);
+    res.json(personRowId)
     })
     .catch((err) => {
       res.status(500).send({
-        message: err.message || "Some error occurred while creating the Captured Image.",
+        message:
+          err.message ||
+          "Some error occurred while creating the Captured Image.",
       });
     });
 };
